3bb67f696f4adaf3cb976d7daeed39d3bd729659
yushinhe <robin124708476@gmail.com>
2021-04-17 21:13:59 +0800

    'formateTime'

--- countDownTime/app.js
+++ countDownTime/app.js
@@ -6,11 +6,14 @@ function countDownTime() {
     const seconds = Math.floor(diff / 1000 % 60);
     const mins = Math.floor(diff / 1000 / 60 % 60);
     const hours = Math.floor(diff / 1000 / 60 / 60 % 24);
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24 ); 
-    document.getElementById('days').innerText = days;
-    document.getElementById('hours').innerText = hours;
-    document.getElementById('mins').innerText = mins;
-    document.getElementById('seconds').innerText = seconds;
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    document.getElementById('days').innerText =days;
+    document.getElementById('hours').innerText =formateTime(hours) ;
+    document.getElementById('mins').innerText =formateTime(mins) ;
+    document.getElementById('seconds').innerText =formateTime(seconds) ;
+}
+function formateTime(time) {
+    return time >= 10 ? time : '0' + time;
 }
 countDownTime()
 setInterval(countDownTime, 1000)
